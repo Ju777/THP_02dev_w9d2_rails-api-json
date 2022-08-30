0fbfe66752287366acf1e7e5aebe6d6662663210
Ju777 <julien.tamil@gmail.com>
2022-08-30 10:15:36 +0200

    Commit because the connexion is very bad.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Article.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('articles')
+
+User.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('users')
+
+5.times do
+    User.create(email: Faker::Internet.email, password:"123456")
+end
+
+30.times do
+    Article.create(title: Faker::DcComics.title, content: Faker::Quote.yoda, user: User.find(rand(1..5)))
+end
