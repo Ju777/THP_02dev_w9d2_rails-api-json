63e1d085edb7d36e4df4fa067bddd8c718053eef
Ju777 <julien.tamil@gmail.com>
2022-08-30 15:13:30 +0200

    Delete a ****** NO FILE ****** migration .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_113200) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_103642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,14 +32,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_113200) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "images", force: :cascade do |t|
-    t.string "image_url"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "jwt_denylist", force: :cascade do |t|
@@ -65,5 +57,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_113200) do
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
-  add_foreign_key "images", "users"
 end
